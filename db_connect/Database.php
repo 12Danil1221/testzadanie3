c836f816b06d7a1c376f1bb586840308654ef948
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2024-12-02 13:48:31 +0500

    Класс Database

--- db_connect/Database.php
+++ db_connect/Database.php
@@ -2,15 +2,22 @@
 
 
 class Database{
-    public $pdo;
+    private $pdo;
 
     public function __construct(){
-        $conn = "mysql:host=localhost;dbname=test";
+        $conn = "mysql:host=localhost;dbname=test2";
         $this->pdo = new PDO($conn, 'root', '');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    public function query($query, $params = []){
+    public function execute($query, $params = []){
         $query = $this->pdo->prepare($query);
         $query->execute($params);
         return $query;
+    }
+    public function beginTransaction(){
+        $this->pdo->beginTransaction();
+    }
+    public function rollback(){
+        $this->pdo->rollBack();
     }
 }
